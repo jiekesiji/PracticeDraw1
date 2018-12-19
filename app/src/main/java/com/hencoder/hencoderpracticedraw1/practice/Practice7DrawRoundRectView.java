a029ed8f39b54ade63f1273abcfbff63f91cc82a
jieke <codesexy@163.com>
2018-12-19 19:07:05 +0800

    添加两个view

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
@@ -2,22 +2,39 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice7DrawRoundRectView extends View {
 
+    private Paint paint;
+    private int width;
+    private int height;
+
     public Practice7DrawRoundRectView(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,-1);
     }
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
     }
 
     @Override
@@ -25,5 +42,7 @@ public class Practice7DrawRoundRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        RectF rectF = new RectF(width / 3, height / 3, width * 2 / 3, height * 2 / 3);
+        canvas.drawRoundRect(rectF,50,50,paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,22 +2,37 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
 
+    private Paint paint;
+    private RectF rectF;
+
     public Practice8DrawArcView(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,-1);
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        rectF = new RectF(w / 3, h / 4, w * 2 / 3, h / 2);
     }
 
     @Override
@@ -25,5 +40,11 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(rectF,10,160,false,paint);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(rectF,180,60,false,paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(rectF,245,100,true,paint);
     }
 }
